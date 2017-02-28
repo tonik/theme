d68606091194b4e36183243ffa1e275f73ef5bf3
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-02-28 16:41:48 +0100

    Adds id argument to the sidebar register function

--- src/Setup/sidebars.php
+++ src/Setup/sidebars.php
@@ -23,6 +23,7 @@ use function {{ theme.namespace }}\config;
 function register_widget_areas()
 {
     register_sidebar([
+        'id' => 'sidebar',
         'name' => __('Sidebar', config('textdomain')),
         'description' => __('Website sidebar', config('textdomain')),
     ]);
