7c323c72100468afd884aae28c02ea8700292cc2
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-03-06 14:05:21 +0100

    Suffix template files with .tpl.php #13

--- config/theme.php
+++ config/theme.php
@@ -1,43 +1,57 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Theme Textdomain
-    |--------------------------------------------------------------------------
-    |
-    | Determines the textdomain for your theme. Should be used to dynamically set
-    | namespace for `gettext` strings across theme. Remember, this value must
-    | be in sync with `Text Domain:` declaration inside style.css theme file.
-    |
-    */
-   'textdomain' => 'theme-textdomain',
+    /**
+     |--------------------------------------------------------------------------
+     | Theme Textdomain
+     |--------------------------------------------------------------------------
+     |
+     | Determines a textdomain for your theme. Should be used to dynamically set
+     | namespace for gettext strings across theme. Remember, this value must
+     | be in sync with `Text Domain:` entry inside style.css theme file.
+     |
+     */
+    'textdomain' => 'theme-textdomain',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Theme Root Paths
-    |--------------------------------------------------------------------------
-    |
-    | This values determines the "root" paths of your theme. By default,
-    | they use WordPress `get_template_directory` functions and
-    | probably you don't need make any changes in here.
-    |
-    */
+    /**
+     |--------------------------------------------------------------------------
+     | Templates files extension
+     |--------------------------------------------------------------------------
+     |
+     | Determines the theme's templates settings like an extension of the files.
+     | By default, they use `.tpl.php` suffix to distinguish template files
+     | from controllers, but you are free to change it however you like.
+     |
+     */
+    'templates' => [
+        'extension' => '.tpl.php'
+    ],
+
+    /**
+     |--------------------------------------------------------------------------
+     | Theme Root Paths
+     |--------------------------------------------------------------------------
+     |
+     | This values determines the "root" paths of your theme. By default,
+     | they use WordPress `get_template_directory` functions and
+     | probably you don't need make any changes in here.
+     |
+     */
     'paths' => [
         'directory' => get_template_directory(),
         'uri' => get_template_directory_uri(),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Theme Directory Structure Paths
-    |--------------------------------------------------------------------------
-    |
-    | This array of directories will be used within core for locating
-    | and loading theme files, assets and templates. They must be
-    | given as relative to the `root` theme directory.
-    |
-    */
+    /**
+     |--------------------------------------------------------------------------
+     | Theme Directory Structure Paths
+     |--------------------------------------------------------------------------
+     |
+     | This array of directories will be used within core for locating
+     | and loading theme files, assets and templates. They must be
+     | given as relative to the `root` theme directory.
+     |
+     */
     'directories' => [
         'languages' => 'resources/languages',
         'templates' => 'resources/templates',
@@ -46,16 +60,16 @@ return [
         'src' => 'src',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Theme Components
-    |--------------------------------------------------------------------------
-    |
-    | The components listed below will be automatically loaded on the
-    | theme bootstrap by `functions.php` file. Feel free to add your
-    | own files to this array which you would like to autoload.
-    |
-    */
+    /**
+     |--------------------------------------------------------------------------
+     | Autoloaded Theme Components
+     |--------------------------------------------------------------------------
+     |
+     | The components listed below will be automatically loaded on the
+     | theme bootstrap by `functions.php` file. Feel free to add your
+     | own files to this array which you would like to autoload.
+     |
+     */
     'autoload' => [
         'helpers.php',
         'Structure/posttypes.php',
--- index.php
+++ index.php
@@ -19,7 +19,7 @@ use function App\Theme\template;
  * Renders [button] shortcode after homepage content.
  *
  * @see do_action('theme/homepage/content/after')
- * @uses resources/templates/shortcodes/button.php
+ * @uses resources/templates/shortcodes/button.tpl.php
  */
 function render_documentation_button()
 {
@@ -30,9 +30,9 @@ add_action('theme/homepage/content/after', 'App\Theme\render_documentation_butto
 /**
  * Renders homepage page.
  *
- * @uses resources/templates/homepage.php
+ * @uses resources/templates/index.tpl.php
  */
-template('homepage', [
+template('index', [
     'title' => __('Tonik — WordPress Starter Theme'),
     'content' => __('Tonik is a WordPress Starter Theme which aims to modernize, organize and simplify some aspects of WordPress theme development. Take a look at what is waiting for you:'),
 ]);
