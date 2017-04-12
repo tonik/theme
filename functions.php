fee67c26a7670e175accf1651e6110d8d872480c
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-04-12 10:19:44 +0200

    Fix path in comment

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 | composer packages, checks compatibility and loads theme files.
 | Most likely, you don't need to change anything in this file.
 | Your theme custom logic should be distributed across a
-| separated components in the `/src` directory.
+| separated components in the `/app` directory.
 |
 */
 
