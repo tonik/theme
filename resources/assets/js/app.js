313b924f3bfaf2134ad42e600a06864639569cc2
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-04-17 17:45:31 +0200

    Import jquery as module because webpack providing it for us

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,4 @@
-/**
- * WordPress bundles a noConflict version of the jQuery.
- * We want tu use it as always, with a dolar sign, so
- * we'll assign jQuery to the `$` variable ourself.
- */
-window.$ = jQuery.noConflict();
+import $ from 'jquery'
 
 /**
  * Lets init Foundation scripts.
