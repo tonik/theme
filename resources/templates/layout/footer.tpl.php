7e9a9856a8b9715ec5c1fc10f3e9b714416a0d45
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-05-15 14:01:46 +0200

    Fill links in footer

--- resources/templates/layout/footer.tpl.php
+++ resources/templates/layout/footer.tpl.php
@@ -3,8 +3,8 @@
                     <footer class="footer">
                         <ul class="menu simple">
                             <li><a href="<?= get_home_url() ?>">Index</a></li>
-                            <li><a href="#">Documentation</a></li>
-                            <li><a href="#">Github</a></li>
+                            <li><a href="https://github.com/tonik/tonik/wiki">Documentation</a></li>
+                            <li><a href="https://github.com/tonik/tonik">Github</a></li>
                         </ul>
                     </footer>
                 </div>
